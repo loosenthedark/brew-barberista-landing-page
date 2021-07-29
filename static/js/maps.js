29e0deb98edcf06dc1c6a2c9c59cfd1e48c4c695
Paul Harrington <hello@loosenthedark.tech>
2021-07-29 19:21:00 +0000

    Fix Maps API geolocation bug with correct lat long coordinates in maps.js

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,5 +1,5 @@
 function initMap() {
-  const brewBarberista = { lat: 53.3810574, lng: -6.1741988 };
+  const brewBarberista = { lat: 53.3811089, lng: -6.1654779 };
   const map = new google.maps.Map(document.querySelector('.map-container'), {
     zoom: 15,
     center: brewBarberista,
